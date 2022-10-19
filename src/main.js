731efdb6fbc0750cb08765bb852689a7946f7107
Jacek Fiszer <jackfiszr+gh@gmail.com>
2022-10-19 19:31:56 +0200

    chore(main.js): import material icons and quasar css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import { Quasar } from "quasar";
 import "./style.css";
+import "@quasar/extras/material-icons/material-icons.css";
+import "quasar/dist/quasar.css";
 import App from "./App.vue";
 
 const myApp = createApp(App);
