eb5df977f62b195858884bba7a3bc6d4f757af0e
Jacek Fiszer <jackfiszr+gh@gmail.com>
2022-10-19 19:35:16 +0200

    chore(main.js): use router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import { createApp } from "vue";
 import { Quasar } from "quasar";
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./router/routes.ts";
 import "./style.css";
 import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/dist/quasar.css";
 import App from "./App.vue";
 
 const myApp = createApp(App);
+
+myApp.use(createRouter({
+  history: createWebHashHistory(),
+  routes,
+}));
 
 myApp.use(Quasar, {
   plugins: {},
