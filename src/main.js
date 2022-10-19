07362928ab8f44ba1a926ee853200ecec4936ab4
Jacek Fiszer <jackfiszr+gh@gmail.com>
2022-10-19 20:10:29 +0200

    feat: add src from quasar cli project

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import { Quasar } from "quasar";
 import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./router/routes.ts";
-import "./style.css";
 import "@quasar/extras/material-icons/material-icons.css";
 import "quasar/dist/quasar.css";
 import App from "./App.vue";
