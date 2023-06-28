caf121c3a9aeccafd1d7fe880c674f378cef8c9d
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 06:46:05 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
-// import { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
-import { Application, Router } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
+// import { Application, Router } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 function api(resource, target){ return {time: new Date().toISOString()} };
 const
   app = new Application(),
@@ -18,17 +18,17 @@ app.use(async (ctx, next) => {
   }
 });
 
-// const router = new Router();
+const router = new Router();
 
 // The /api/time endpoint returns the current time in ISO format.
-app.get("/:path/:ressource?/:id?", async (ctx) => {
-  ctx.response.body = await ctx.params.path === "api" ? api() : ctx.file(root+'/index.html')
+router.get("/:path/:ressource?/:id?", async (ctx) => {
+  ctx.response.body = await ctx.params.path === "api" ? api() : ctx.send({path: root+'/index.html'})
   console.log(ctx.response.body)
 });
 
 // After creating the router, we can add it to the app.
-// app.use(router.routes());
-// app.use(router.allowedMethods());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
-// await app.listen({ port: 8000 });
-await app.start({ port: 8000 });
+await app.listen({ port: 8000 });
+// await app.start({ port: 8000 });
