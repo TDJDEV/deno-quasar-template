a1f28dc888238ba555af914b03c61695a77d9e05
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 04:00:37 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -10,7 +10,12 @@
 // app.listen(8080);
 
 import { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
-const app = new Application();
+const
+  app = new Application(),
+  routeBool = {
+    true(resource, target){ return {body: { time: new Date().toISOString() }}  },
+    false(){ return {body: Deno.readFile(`${Deno.cwd()}/dist/index.html`), type: "text/html"}  }
+  };
 
 // First we try to serve static files from the _site folder. If that fails, we
 // fall through to the router below.
@@ -29,7 +34,7 @@ const router = new Router();
 
 // The /api/time endpoint returns the current time in ISO format.
 router.get("/:path/:ressource?/:id?", async (ctx) => {
-  ctx.response.body = await ctx.params.path === "api" ? { time: new Date().toISOString() } : Deno.readFile(`${Deno.cwd()}/dist/index.html`)
+  Object.assign(ctx.response.body, await routeBool[ctx.params.path === "api"]()); 
 });
 
 // After creating the router, we can add it to the app.
