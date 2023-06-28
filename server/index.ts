6720ad5cbe4ff303c8bd068d1d66dcda6f1f5527
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 01:44:41 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,13 @@
 import express from "https://esm.sh/express?target=denonext";
+import index from "/dist/index.html";
 const app = express();
 app.use(express.static('/dist'))
 app.use(express.static('/public'))
-
+console.log(index)
 app.get("/api", (req, res) => {
   res.send("Hello from Deno Deploy!");
 });
 app.get("*", (req, res) => {
-  res.sendFile("../dist/index.html");
+  res.sendFile("/dist/index.html");
 });
 app.listen(8080);
