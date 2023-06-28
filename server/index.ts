7469db96a86c7b6aa58ba9c63335006b9e2651e0
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 00:31:44 -0400

    add webapp test

---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+
+const app = new Application();
+
+console.log("http://localhost:8080/");
+
+app
+  .get("/hello", (c) => {
+  return "Hello, Abc!";
+  })
+  .get("/", (c) => {
+  return "Hello, Abc!";
+  })
+  .start({ port: 8080 });
