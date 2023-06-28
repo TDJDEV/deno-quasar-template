7cda358076b2f08546f9771132b6d2b8365b6c33
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 07:25:43 -0400

    set server routing

--- server/index.ts
+++ server/index.ts
@@ -1,35 +1,31 @@
+// Imports
 import { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
-// import { Application, Router } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+
+// Functions
 function api(resource, target){ return {time: new Date().toISOString()} };
+
+// Constants
 const
   app = new Application(),
   root = `${Deno.cwd()}/dist`,
   decoder = new TextDecoder("utf-8"),
   data = await Deno.readFile(root+'/index.html');
+
+// Process
+
 // First we try to serve static files from the _site folder. If that fails, we
 // fall through to the router below.
 app.use(async (ctx, next) => {
-  try {
-    await ctx.send({
-      root,
-      index: "index.html",
-    });
-  } catch {
-    next();
-  }
+  try { await ctx.send({ root, index: "index.html"}); }
+  catch { next(); }
 });
 
 const router = new Router();
 
-// The /api/time endpoint returns the current time in ISO format.
-router.get("/:path/:ressource?/:id?", async (ctx) => {
-  ctx.response.body = await ctx.params.path === "api" ?  api() : decoder.decode(data)
-  console.log(ctx.response.body)
-});
+router.get("/:path/:ressource?/:id?", async (ctx) => { ctx.response.body = await ctx.params.path === "api" ?  api() : decoder.decode(data) });
 
 // After creating the router, we can add it to the app.
 app.use(router.routes());
 app.use(router.allowedMethods());
 
 await app.listen({ port: 8000 });
-// await app.start({ port: 8000 });
