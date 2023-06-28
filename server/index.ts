8d7d32196fde4914cf3ae48c84c4835bbb143728
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 01:52:05 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -8,6 +8,6 @@ app.get("/api", (req, res) => {
   res.send("Hello from Deno Deploy!");
 });
 app.get("*", (req, res) => {
-  res.sendFile("/dist/index.html");
+  res.sendFile(current+"dist/index.html");
 });
 app.listen(8080);
