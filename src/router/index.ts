9f57ab5bc9c28373f1d7b857fcd1e634c1915ca8
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-28 00:00:25 -0400

    routes test

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const
       children: [{ path: "", component: () => import("../pages/IndexPage.vue") }],
     },
 
+    {
+      path: "/toto",
+      component: () => import("../layouts/MainLayout.vue"),
+      children: [{ path: "", component: () => import("../pages/IndexPage.vue") }],
+    },
     // Always leave this as last one,
     // but you can also remove it
     {
