b3be876630d0d51292a09c48ef8479d053dc96bc
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-29 12:34:21 -0400

    add api

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,14 @@ const
   app = new Application(),
   root = `${Deno.cwd()}/dist`,
   decoder = new TextDecoder("utf-8"),
-  data = await Deno.readFile(root+'/index.html');
+  data = await Deno.readFile(root+'/index.html'),
+  store = {},
+  api = {
+    create(key:string){ return (store[key] || (store[key]=new Map)).set(`${key}-${store[key].lenght}`, { id:`${key}-${store[key].lenght}`, collection:key, time:new Date().toISOString()}) },
+    read(key:string,id:string){ return id ? store[key]?.get(id) : store[key]?.values() },
+    update(key:string,id:string){ return store[key]?.set(id,{ id, collection:key, time:new Date().toISOString()})},
+    delete(key:string,id:string){ return {msg: store[key]?.delete(id) ? `item id:${id} has been removed`: `error: cannot remove item id:${id}`} }
+  };
 
 // Process
 
@@ -22,7 +29,11 @@ app.use(async (ctx, next) => {
 
 const router = new Router();
 
-router.get("/:path/:ressource?/:id?", async (ctx) => { ctx.response.body = await ctx.params.path === "api" ?  api() : decoder.decode(data) });
+router.get("/api/resources",          async (ctx) => { ctx.response.body = await                              Object.keys(store));
+router.post("/api/:resource/",        async (ctx) => { ctx.response.body = await                              api.create(ctx.params.ressource) });
+router.get("/:path/:resource?/:id?",  async (ctx) => { ctx.response.body = await ctx.params.path === "api" ?  api.read(ctx.params.ressource,ctx.params.id) : decoder.decode(data) });
+router.put("/api/:resource/:id",      async (ctx) => { ctx.response.body = await                              api.update(ctx.params.ressource,ctx.params.id) });
+router.delete("/api/:resource/:id",   async (ctx) => { ctx.response.body = await                              api.delete(ctx.params.ressource,ctx.params.id) });
 
 // After creating the router, we can add it to the app.
 app.use(router.routes());
