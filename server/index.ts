650f2bffb20ee60f9855aa37452db333f939e8df
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-06-29 18:07:36 -0400

    fix: api.create

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { Application, Router } from "https://deno.land/x/oak@v10.2.0/mod.ts";
 
 // Functions
 function logs(ctx){ console.log(ctx.request) };
-function createRecord(collection,id){ return collection.set(id,{ id, collection, createAt:new Date().toISOString()}) };
+function createRecord(table,collection,id){ return table.set(id,{ id, collection, createAt:new Date().toISOString()}) };
 function updateRecord(record){ return record && (record.updatedAt = new Date().toISOString()) };
 async function setWrapper(fn) { console.log(fn); return (ctx, next)=>{logs(ctx, next), fn(ctx, next)} }
 
@@ -15,7 +15,7 @@ const
   data = await Deno.readFile(root+'/index.html'),
   store = {},
   api = {
-    create(key:string)            { return { msg: createRecord(store[key]||(store[key]=new Map), `${key}-${store[key].lenght}`) ? `new item has been created with id:${key}-${store[key].lenght}`: `error: cannot create new item`} },
+    create(key:string)            { return { msg: createRecord(store[key]||(store[key]=new Map), key, `${key}-${store[key].size}`) ? `new item has been created with id:${key}-${store[key].size}`: `error: cannot create new item`} },
     read(key:string,id:string)    { return id ? store[key]?.get(id) : store[key]?.values() || {msg: 'not found'} },
     update(key:string,id:string)  { return { msg: updateRecord(store[key]?.get(id)) ? `item id:${id} has been updated`: `error: cannot update item id:${id}`} },
     delete(key:string,id:string)  { return { msg: store[key]?.delete(id) ? `item id:${id} has been removed`: `error: cannot remove item id:${id}`} }
