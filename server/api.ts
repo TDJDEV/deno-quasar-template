78cb0ca1bf8cb51bf12a1cc6426a50ad42a37bd9
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-07-03 03:08:46 -0400

    add: Record class

--- server/api.ts
+++ server/api.ts
@@ -2,32 +2,43 @@ class Record {
   #__dataModel__
   #__data__
   #__attributes__
+  #__map__
 
-  constructor(id,model={},data={}){
+  constructor(model={},data={}){
     this.#__dataModel__ = model
-    this.#__data__ = {
-      id,
-      updatedAt: 'never' || new Date().toISOString(),
+    this.#__map__ = {
+      id:'id',
+      collection: 'collection',
+      createdAt: 'creationDate',
+      updatedAt: 'lastUpdate',
+      attributes: 'attributes' 
     }
-    // for(const [key, val] of Object.entries(model)) this.#__data__[key] = val;
-    this.set(data)
+
+    this.#__data__ = { set attributes(val){} }
+		this.#setMeta(data)
+		this.set(data.attributes)
   }
 
   // getters
-  get id()          { return this.#__data__.id }
-  get creation()    { return this.#__data__.createAt }
-  get lastUpdate()  { return this.#__data__.updatedAt }
-  get attributes()  { return {...this.#__attributes__} }
+  get id()            { return this.#__data__.id }
+  get collection()    { return this.#__data__.collection }
+  get creationDate()  { return this.#__data__.createAt }
+  get lastUpdate()    { return this.#__data__.updatedAt }
+  get attributes()    { return {...this.#__attributes__} }
 
   // public methods
-  set(data:object)    { this.#setDate(this.#__attributes__ ? 'updatedAt': 'createdAt'), this.#__attributes__ = this.#cloneData(data) }
-  update(data:object) { ((attrs)=>Object.entries(this.#cloneData(data)).forEach(([key,val])=>attrs[key]=val))(this.#__attributes__) }
+  set(data:object)    { return (str=>this.#isValid(data) && (this.#__attributes__ = this.#cloneData(data)) && this.#setDate(str))(this.#__attributes__ ? 'updatedAt': 'createdAt') }
+  update(data:object) { return ((attrs)=>Object.entries(this.#cloneData(data)).forEach(([key,val])=>attrs[key]=val))(this.#__attributes__), true }
 
   // private methods
+  #setMeta(data)                { Object.entries(this.#__map__).map(([name,label]) => this.#__data__[name]=(data[label])?.toString()) }
   #cloneData(data:object)       { return JSON.parse(JSON.stringify(data||{})) }
-  #setDate(key:string)          { this.#__data__[key] = new Date().toISOString() }
-  #checkAttributes(data:object)  { return Object.entries(this.#__dataModel__).reduce((o,[key,type])=>((val)=>this.#checkDataType(val,type) ? (o[key]=val) : o)(data[key]),{}) }
+  #setDate(key:string)          { return this.#__data__[key] = new Date().toISOString() }
+  #checkAttributes(data:object) { return Object.entries(this.#__dataModel__).reduce((o,[key,type])=>((val)=>this.#checkDataType(val,type) ? (o[key]=val) : o)(data[key]),{}) }
   #checkDataType(val,type)      { return [type,typeof type].includes(typeof val) }
+  #isValid(data:object={})      { return typeof data === "object" && !Array.isArray(data) }
+
+  toJSON(){ return Object.values(this.#__map__).reduce((acc,key) => ((acc[key]=this[key]),acc),{})  }
 }
 
 class Collection{
@@ -44,13 +55,14 @@ class Collection{
   }
 
   get name() { return this.#__name__ }
-  create(){ return this.#createRecord(this.#__data__, this.#__name__, this.#createUID()) }
+  create(data:unknown){ return ((id:string)=>this.#add(this.#createRecord(this.#__name__, id, data)) )(this.#createUID()) }
   read(id:string, filters:any)   { return id ? this.#__data__?.get(id) : this.#filter(this.#toArray(this.#__data__?.values()), filters) }
-  update(id:string) { return this.#patchRecord(this.#__data__.get(id)) ? `item id:${id} has been updated`: `error: cannot update item id:${id}`}
-  delete(id:string) { return this.#__data__.delete(id) ? `item id:${id} has been removed`: `error: cannot remove item id:${id}`}
-  patch(id:string)  { return this.#__data__.get(id) ? `item id:${id} has been patched`: `error: cannot patch item id:${id}` }
+  update(id:string) { return this.#__data__.get(id)?.set(data)}
+  delete(id:string) { return this.#__data__.delete(id)}
+  patch(id:string)  { return this.#__data__.get(id)}
   
-  #createRecord(table,collection,id){ return table.set(id,{ id, collection, createAt:new Date().toISOString()}) ? `new item has been created with id:${id}`:`error: cannot create new item` }
+  #createRecord(collection:string,id:string, attributes:unknown){ return new Record(undefined,{id,collection, attributes})  }
+  #add(record){ return this.#__data__.set(record.id, record) }
   #patchRecord(record){ return record && (record.updatedAt = new Date().toISOString()) };
   #createUID(){ return ((char,charLen)=>(new Array(7)).fill().reduce((id)=>id+char.charAt(Math.floor(Math.random() * charLen)),this.#__i__++))(this.#__chars__,this.#__chars__.length) }
   #toArray(item){ return item && [...item] };
@@ -74,10 +86,9 @@ export class Store {
 }
 
 export class Api extends Store {
-  #__store__
   #__from__
   #__to__
-  #__dataDummy__
+  #__fn__
 
   constructor({router,path=""}) {
     super()
@@ -86,12 +97,12 @@ export class Api extends Store {
     "/" !== path.slice(-1) && (path+="/"); 
 
     // Api routes
-    router.get(`/${path}export`,             async (ctx) => { ctx.response.body = await this.json });         // return db to json
-    router.get(`/${path}collections`,        async (ctx) => { ctx.response.body = await this.collections });  // Return collections list
-    router.post(`/${path}:collection`,       async (ctx) => { ctx.response.body = await this.action(ctx.params.collection,'create') });
-    router.get(`/${path}:collection/:id?`,   async (ctx) => { ctx.response.body = await this.action(ctx.params.collection,'read',ctx.params.id) });
-    router.put(`/${path}:collection/:id`,    async (ctx) => { ctx.response.body = await this.action(ctx.params.collection,'update',ctx.params.id) });
-    router.delete(`/${path}:collection/:id`, async (ctx) => { ctx.response.body = await this.action(ctx.params.collection,'delete',ctx.params.id) });
+    router.get(`/${path}export`,             async (ctx) => { ctx.response.body = await this.action('export') });  // return db to json
+    router.get(`/${path}collections`,        async (ctx) => { ctx.response.body = await this.action('list') });    // Return collections list
+    router.post(`/${path}:collection`,       async (ctx) => { ctx.response.body = await this.action('create',ctx.params.collection) });
+    router.get(`/${path}:collection/:id?`,   async (ctx) => { ctx.response.body = await this.action('read',ctx.params.collection,ctx.params.id) });
+    router.put(`/${path}:collection/:id`,    async (ctx) => { ctx.response.body = await this.action('update',ctx.params.collection,ctx.params.id) });
+    router.delete(`/${path}:collection/:id`, async (ctx) => { ctx.response.body = await this.action('delete',ctx.params.collection,ctx.params.id) });
     
     // Convert data from Object to selected format
     this.#__from__={
@@ -102,6 +113,11 @@ export class Api extends Store {
     this.#__to__={
       json(data){ return JSON.stringify(data) }
       // sql(){  }
+    }
+
+    this.#__fn__={
+      list(){ return this.collections },
+      export(){ return this.json }
     }
   }
   
@@ -121,11 +137,17 @@ export class Api extends Store {
   /******************
   *     Methods     *
   ******************/
+  //request handler
+  action(action:string, name:string, ...args:any[]) { return Object.keys(this.#__fn__).includes(action) ? this.#__fn__[action] : super.action(name,action,...args) }
   // Return an object store data in the passed format if known
   #import(format:string, data:unknown)     { super.data = this.#convertData(this.#__from__[format], data) }
   // Return store data in the passed format if known  
   #export(format:string)                  { return this.#convertData(this.#__to__[format], super.data) }
   // Return converted data if the converter exist  
   #convertData(fn:Function, data:unknown) { return fn?fn(data):"unknown format"  }
+  // msg generator
+  #actionLog(res:unknown,action:string, id:string){ return (res ? this.#successMsg : this.#errorMsg)(action, id,res) }
+  #successMsg(action:string, id:string, res:unknown) { return action=='read' ? res : `Item id:${id} has been ${action}d` }
+  #errorMsg(action:string, id:string) { return `Error => cannot ${action} item${id ? ' id:'+id : ''}` }
   
 }
