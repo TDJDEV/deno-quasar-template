5f20ceee8f60f647890b214af74f5d2539138fda
TDJDEV <56182372+TDJDEV@users.noreply.github.com>
2023-07-04 11:25:08 -0400

    tidying

--- server/api.ts
+++ server/api.ts
@@ -1,12 +1,13 @@
+// 
 class Record {
   #__dataModel__
-  #__data__
+  #__attrsModel__
+  #__meta__
   #__attributes__
-  #__map__
 
   constructor(model={},data={}){
-    this.#__dataModel__ = model
-    this.#__map__ = {
+    this.#__attrsModel__ = model
+    this.#__dataModel__ = {
       id:'id',
       collection: 'collection',
       createdAt: 'creationDate',
@@ -14,43 +15,67 @@ class Record {
       attributes: 'attributes' 
     }
 
-    this.#__data__ = { set attributes(val){} }
-		this.#setMeta(data)
+    this.#__meta__ = { set attributes(val){} }
+		this.#setMeta(this.#parseMeta(data))
 		this.set(data.attributes)
-    console.log(this.#__data__)
+    console.log(this.#__meta__)
   }
 
   // getters
-  get id()            { return this.#__data__.id }
-  get collection()    { return this.#__data__.collection }
-  get creationDate()  { return this.#__data__.createAt }
-  get lastUpdate()    { return this.#__data__.updatedAt }
+  get id()            { return this.#__meta__.id }
+  get collection()    { return this.#__meta__.collection }
+  get creationDate()  { return this.#__meta__.createAt }
+  get lastUpdate()    { return this.#__meta__.updatedAt }
   get attributes()    { return {...this.#__attributes__} }
 
-  // public methods
-  set(data:object)    { return (str=>this.#isValid(data) && (this.#__attributes__ = this.#cloneData(data)) && this.#setDate(str))(this.#__attributes__ ? 'updatedAt': 'createdAt') }
-  update(data:object) { return ((attrs)=>Object.entries(this.#cloneData(data)).forEach(([key,val])=>attrs[key]=val))(this.#__attributes__), true }
+  /*  public methods  */
+  // #set public accesses
+  set(data:object)    { this.#set(this.#isValid(data) && this.#parse(data),this.#__attributes__) }
+  // #update public accesses
+  update(data:object) { return this.#update(this.#isValid(data) && this.#parse(data)) }
 
-  // private methods
-  #setMeta(data)                { Object.entries(this.#__map__).map(([name,label]) => this.#__data__[name]=(data[label])?.toString()) }
-  #cloneData(data:object)       { return JSON.parse(JSON.stringify(data||{})) }
-  #setDate(key:string)          { return this.#__data__[key] = new Date().toISOString() }
-  #checkAttributes(data:object) { return Object.entries(this.#__dataModel__).reduce((o,[key,type])=>((val)=>this.#checkDataType(val,type) ? (o[key]=val) : o)(data[key]),{}) }
-  #checkDataType(val,type)      { return [type,typeof type].includes(typeof val) }
-  #isValid(data:object={})      { return typeof data === "object" && !Array.isArray(data) }
+  /*  private methods  */
 
-  toJSON(){ return Object.values(this.#__map__).reduce((acc,key) => ((acc[key]=this[key]),acc),{})  }
+  // set record meta data
+  #setMeta(data)                                          { this.#__meta__ = this.#cloneData(data)}
+  // set record attributes
+  #set(data:object,isNew:boolean)                         { return data && (this.#__attributes__ = this.#cloneData(data)) && this.#setDate(isNew) }
+  // Return a conform meta-data structure
+  #parseMeta(data:object)                                 { return this.#browse(this.#__dataModel__,this.#reducer(this.#addMeta,data),{}) }
+  // Return a conform attribute structure
+  #parse(data:object)                                     { return this.#browse(this.#__attrsModel__,this.#reducer(this.#check,data),{})}
+  // Add data to record meta data
+  #addMeta(o:object,[name,label]:string[],data:object)    { return (o[name]=data[label]), o }
+  // add authorized attributes
+  #check(o:object,[key,type]:[string,string],data:object) { return ((val:unknown) => this.#checkDataType(val,type) && (o[key]=val))(data[key]), o }
+  //Clone data deeply
+  #cloneData(data:object)                                 { return JSON.parse(JSON.stringify(data||{})) }
+  // Set mutation date
+  #setDate(isNew:boolean)                                 { return this.#__meta__[isNew ? 'createdAt' : 'updatedAt'] = new Date().toISOString() }
+  // Reduce callback supercharger
+  #reducer(fn:Function, ...datas:unknown[])               { return (acc:any, it:unknown)=>fn(acc, it, ...datas) }
+  // check attribute data type
+  #checkDataType(val,type)                                { return [type,typeof type].includes(typeof val) }
+  // check if passed data is an object
+  #isValid(data:object={})                                { return typeof data === "object" && !Array.isArray(data) }
+  // Browse object and return accumulator if passed
+  #browse(data:object,fn:Function,o:any)                  { return Object.entries(data)[o ? 'reduce' : 'forEach'](fn,o) }
+  // Partially update attributes
+  #update(data:object)                                    { return data && Object.assign(this.#__attributes__ ,this.#cloneData(data))}  
+
+  // Set JSONable structure
+  toJSON(){ return Object.values(this.#__dataModel__).reduce((acc,key) => ((acc[key]=this[key]),acc),{})  }
 }
 
 class Collection{
   #__name__
-  #__data__
+  #__meta__
   #__chars__
   #__i__
 
   constructor(name,data){
     this.#__name__ = name
-    this.#__data__ = new Map(data?.map(x=>[x.id,x]))
+    this.#__meta__ = new Map(data?.map(x=>[x.id,x]))
     this.#__chars__ = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     this.#__i__ = 0
   }
@@ -61,42 +86,76 @@ class Collection{
   *     Methods     *
   ******************/
 
+  /* public */
+
+  // crud
   create(data:unknown)            { return ((id:string)=>this.#add(this.#createRecord(this.#__name__, id, data)) )(this.#createUID()) }
-  read(id:string, filters:any)    { return id ? this.#__data__?.get(id) : this.#filter(this.#toArray(this.#__data__?.values()), filters) }
-  update(id:string,data:unknown)  { return this.#__data__.get(id)?.set(data)}
-  delete(id:string)               { return this.#__data__.delete(id)}
-  patch(id:string)                { return this.#__data__.get(id)}
+  read(id:string, filters:any)    { return id ? this.#__meta__?.get(id) : this.#filterHandler(this.#toArray(this.#__meta__?.values()), filters) }
+  update(id:string,data:unknown)  { return this.#__meta__.get(id)?.set(data)}
+  delete(id:string)               { return this.#__meta__.delete(id)}
+  patch(id:string)                { return this.#__meta__.get(id)}
   
-  #createRecord(collection:string,id:string, attributes:unknown){ return new Record(undefined,{id,collection, attributes})  }
-  #add(record){ return (id=>(this.#__data__.set(id, record),id))(record.id) }
-  #patchRecord(record){ return record && (record.updatedAt = new Date().toISOString()) };
-  #createUID(){ return ((char,charLen)=>(new Array(7)).fill().reduce((id)=>id+char.charAt(Math.floor(Math.random() * charLen)),this.#__i__++))(this.#__chars__,this.#__chars__.length) }
-  #toArray(item){ return item && [...item] };
-  #filter(data, filters){ return filters ? ((filters)=>data.filter(record => filters.every(([key,val])=>record[key]==val)))(Object.entries(filters)) : data }
+  /* private */
+
+  // Create a record
+  #createRecord(collection:string,id:string, attributes:unknown)  { return new Record(undefined,{id,collection, attributes})  }
+  // Add recorde to collection
+  #add(record:Record)                                             { return (id=>(this.#__meta__.set(id, record),id))(record.id) }
+  // Partially update record
+  #patchRecord(record:Record, data:unknown)                       { return record?.update(data) }
+  // Generate an id
+  #createUID()                                                    { return this.#loop(7,(id)=>id+this.#getRandom(this.#__chars__),this.#__i__++) }
+  // Return value from random index 
+  #getRandom(datas:string|[])                                     { return datas && datas[Math.floor(Math.random() * datas.length)] }
+  // Convert iterable to array
+  #toArray(item:Iterable)                                         { return item && [...item] };
+  // Iterate on the passed method 
+  #loop(n:number,fn:Function,acc:any)                             { return (new Array(n)).fill().reduce(fn,acc)}
+  // Return filtered records
+  #filterHandler(data:Record[], filters:{})                       { return filters ? this.#Filter(data,this.#readFilters(filters)) : data }
+  // Convert filters to iterable
+  #readFilters(filters:{})                                        { return Object.entries(filters) }
+  // Filter collection records
+  #Filter(data:{}[],filters:[][])                                 { return data.filter(record => filters.every(([key,val])=>record[key]==val)) }
 }
 
 export class Store {
   #__collections__
 
-  constructor() { this.#__collections__ = new Map }
+  constructor(data) { this.#__collections__ = this.#init(data) }
 
   /******************
   * Getters/Setters *
   ******************/
   // Return collection list
-  get collections(){ return [...this.#__collections__.keys()] }
+  get collections()     { return [...this.#__collections__.keys()] }
 
   // Store datas
-  get data() { return Object.fromEntries(this.collections.map((key)=>[key,this.#action(key,'read')])) }
-  set data(dataObject:string){ this.#__collections__ = new Map(Object.entries(dataObject).map((collectionData)=>new Collection(...collectionData))) }
+  get data()            { return Object.fromEntries(this.collections.map((key)=>[key,this.#action(key,'read')])) }
+  set data(dataObject)  { this.#__collections__ = this.#init(dataObject) }
 
   /******************
   *     Methods     *
   ******************/
 
-  action(name:string, action:string, ...args:any[]){ return this.#action(name, action, args) }
-  #action(name:string, action:string, args:any[]){ return ((table:Collection) => table && table[action](...args))(this.#getCollection(name, action === "create")) }
-  #getCollection(name:string, create:boolean){ return ((db:Map)=> (db.has(name)) && db || (create ? db.set(name,new Collection(name)): null) )(this.#__collections__)?.get(name) }
+  // Public access to #action
+  action(name:string, action:string, ...args:any[])   { return this.#action(name, action, ...args) }
+  // Return requested data if found 
+  #action(name:string, action:string, ...args:any[])  { return this.#do(this.#get(name, action === "create"), action, args) }
+  // Return collection or undefined
+  #get(name:string, create:boolean)                   { return this.#addOrFind(this.#__collections__)?.get(name) }
+  // Set or replace store data
+  #init(data={})                                      { return new Map(Object.entries(data).map((arr)=>this.#create(...arr))) }
+  // Create a collection
+  #create(name:string,data:[])                        { return console(`Collection ${name} has been created`), new Collection(name,data) }
+  // Find a collection or add a new one
+  #addOrFind(db:Map, name:string, create:boolean)     { return this.#find(db) || create ? this.#add(db, name) : null }
+  // Add a new collection to store map
+  #add(db:Map, name:string)                           { return db.set(name,this.#create(name)) }
+  // find a collection
+  #find(db:Map)                                       { return db.has(name) && db }
+  // Execute the passed action
+  #do(table:Collection, action, args:any[])           { return table && table[action](...args) }
 }
 
 export class Api extends Store {
@@ -152,16 +211,23 @@ export class Api extends Store {
   *     Methods     *
   ******************/
   //request handler
-  action(action:string, name:string, ...args:any[]) { return Object.keys(this.#__fn__).includes(action) ? this.#__fn__[action] : this.#actionLog(super.action(name,action,...args),action,...args) }
+  action(action:string, name:string, ...args:any[])   { return this.#actionLog(this.#handler(action,name)(...args),action,...args) }
   // Return an object store data in the passed format if known
-  #import(format:string, data:unknown)     { super.data = this.#convertData(this.#__from__[format], data) }
+  #handler(action:string, name:string)                { return this.#apiAction(action) || this.#storeAction(action, name) }
+  // return api action handler
+  #apiAction(action:string)                           { return Object.keys(this.#__fn__).includes(action) && (()=>this.#__fn__[action]()) }
+  // return store action handler
+  #storeAction(action:string, name:string)            { return args=>super.action(name,action,...args) }
+  #import(format:string, data:unknown)                { super.data = this.#convertData(this.#__from__[format], data) }
   // Return store data in the passed format if known  
-  #export(format:string)                  { return this.#convertData(this.#__to__[format], super.data) }
+  #export(format:string)                              { return this.#convertData(this.#__to__[format], super.data) }
   // Return converted data if the converter exist  
-  #convertData(fn:Function, data:unknown) { return fn?fn(data):"unknown format"  }
+  #convertData(fn:Function, data:unknown)             { return fn?fn(data):"unknown format"  }
   // msg generator
-  #actionLog(res:unknown,action:string, id:string){ return (res ? this.#successMsg : this.#errorMsg)(action, id,res) }
-  #successMsg(action:string, id:string, res:unknown) { return action=='read' ? res : {msg: `Item id:${id || res} has been ${action}d`} }
-  #errorMsg(action:string, id:string) { return {error: action=='read' ? undefined :`cannot ${action} item${id ? ' id:'+id : ''}`} }
+  #actionLog(res:unknown,action:string, id:string)    { return (res ? this.#successMsg : this.#errorMsg)(action, id,res) }
+  // Generate a success message
+  #successMsg(action:string, id:string, res:unknown)  { return action=='read' ? res : {msg: `Item id:${id || res} has been ${action}d`} }
+  // Generate an error message
+  #errorMsg(action:string, id:string)                 { return {error: action=='read' ? undefined :`cannot ${action} item${id ? ' id:'+id : ''}`} }
   
 }
